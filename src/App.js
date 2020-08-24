c3943cf5d9815f5eeb3c7f71271e3a131089d9c1
bombchu <kafesey@gmail.com>
2020-08-24 19:19:29 +1000

    testing out Amplify save to s3 functionality

--- src/App.js
+++ src/App.js
@@ -2,7 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { withAuthenticator } from 'aws-amplify-react'
+import { Storage } from 'aws-amplify'
+
 class App extends Component {
+	state = { fileUrl: '', file: '', filename: '' }
+	handleChange = e => {
+		const file = e.target.files[0]
+		this.setState({
+			fileUrl: URL.createObjectURL(file),
+			file,
+			filename: file.name
+		})
+	}
+	saveFile = () => {
+		Storage.put(this.state.filename, this.state.file).then( () => {
+			console.log('saved file.')
+			this.setState( fileUrl: '', file: '', filename: '' })
+	})
+		.catch(err => {
+			console.log('error uploading file.', err)
+		})
+}
   render() {
     return (
       <div className="App">
@@ -13,6 +34,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+	    <input type='file' onChange={this.handleChange} />
+	    <img src={this.state.fileUrl} />
+	    <button onClick={this.saveFile}>Save ya ting</button>
       </div>
     );
   }
